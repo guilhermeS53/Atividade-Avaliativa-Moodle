a20c219670204fa27df0a48aa5ed18ce06685b7b
Guilherme Sousa <guilherme.gs17@outlook.com>
2023-05-06 10:08:04 -0300

    initial commit

--- src/Ex01/Ex1.java
+++ src/Ex01/Ex1.java
@@ -1,6 +1,17 @@
+package Ex01;
+
+import javax.swing.*;
+
 public class Ex1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int qtdMin, qtdMax;
+        double estoqueMedio;
 
+        qtdMin = Integer.parseInt(JOptionPane.showInputDialog("Informe a quantidade mínima da peça: "));
+        qtdMax = Integer.parseInt(JOptionPane.showInputDialog("Informe a quantidade mínima da peça: "));
+
+        estoqueMedio = (qtdMin + qtdMax) / 2;
+
+        JOptionPane.showMessageDialog(null, "De acordo com o informado, o estoque médio dessa peça é: " + "\n" + estoqueMedio);
     }
 }
