0fd78f43d0e94eac5a76f9e79693064ca324503f
Guilherme Sousa <guilherme.gs17@outlook.com>
2023-05-07 14:56:07 -0300

    ..

--- src/Ex05/Ex5.java
+++ src/Ex05/Ex5.java
@@ -7,10 +7,12 @@ public class Ex5 {
 
         int x;
         x = Integer.parseInt(JOptionPane.showInputDialog("Informe um valor qualquer para X: "));
-        System.out.println("O valor informado para X foi: " + x);
+        System.out.println("O valor informado para X foi " + x);
 
         if (x >= 0) {
-            System.out.println("O módulo do valor informado para X é: ");
+            System.out.println("O módulo do valor informado para X é: " + x);
+        } else {
+            System.out.println("O módulo do valor informado para X é: " + x * (-1));
         }
     }
 }
