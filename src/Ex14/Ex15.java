1dfe43bc3c46f2e9c477eff87469fd3944021c6f
Guilherme Sousa <guilherme.gs17@outlook.com>
2023-05-13 21:53:29 -0300

    Execução do Ex.15

---
+++ src/Ex14/Ex15.java
@@ -0,0 +1,40 @@
+package Ex14;
+
+import javax.sound.midi.Soundbank;
+import javax.swing.*;
+import java.sql.SQLOutput;
+
+public class Ex15 {
+    public static void main(String[] args) {
+        int[] v = new int[2];
+        int x;
+        int maior, menor, igual;
+        // declaro todos inicialmente valendo 0
+        maior = menor = igual = 0;
+
+        for (int i = 0; i < v.length; i++) {
+            String ent = JOptionPane.showInputDialog("Informe um número para o array de 10 posições: " + (i+1));
+            System.out.println("Os números informados foram: " + i);
+            v[i] = Integer.parseInt(ent);
+        }
+
+        System.out.println("\n");
+        String xString = JOptionPane.showInputDialog("Informe o número X: ");
+        System.out.println("O valor informado de X foi " + xString);
+        x = Integer.parseInt(xString);
+
+        for (int i = 0; i < v.length; i++) {
+            if (v[i] > x) {
+                maior++;
+            } else
+            if (v[i] < x) {
+                menor++;
+            } else {
+                igual++;
+            }
+        }
+        System.out.println("Quantidade de números maiores que X: " + maior);
+        System.out.println("Quantidade de números menores que X: " + menor);
+        System.out.println("Quantidade de números iguais a X: " + igual);
+    }
+}
